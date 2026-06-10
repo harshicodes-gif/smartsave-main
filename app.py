fbbff5eaa812c6e6fde679eafba03fcb1badfa1f
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-10 14:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-import sys
-import os
-
-sys.path.append(
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            ".."
-        )
-    )
-)
-
 import streamlit as st
 
-from backend.database.db import init_db
+from db import init_db
 
-from frontend.pages.dashboard import show_dashboard
-from frontend.pages.analytics import show_analytics
-from frontend.pages.ai_coach import show_ai_coach
-from frontend.pages.savings_game import show_game
+from dashboard import show_dashboard
+from analytics import show_analytics
+from ai_coach import show_ai_coach
+from savings_game import show_game
 
 init_db()
 
