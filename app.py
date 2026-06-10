06e5c712ab87931cec97d5f9dd99c0f99af82e2d
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-10 14:54:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 from db import init_db
-
 from dashboard import show_dashboard
 from analytics import show_analytics
 from ai_coach import show_ai_coach
@@ -15,10 +14,25 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("💰 SmartSave")
+st.markdown("""
+<h1 style='text-align:center;'>
+💰 SmartSave
+</h1>
 
-menu = st.sidebar.selectbox(
-    "Navigation",
+<h4 style='text-align:center;color:gray;'>
+Manage Pocket Money Smarter
+</h4>
+""", unsafe_allow_html=True)
+
+st.image(
+    "https://images.unsplash.com/photo-1554224155-6726b3ff858f",
+    use_container_width=True
+)
+
+st.sidebar.title("Navigation")
+
+menu = st.sidebar.radio(
+    "",
     [
         "Dashboard",
         "Analytics",
