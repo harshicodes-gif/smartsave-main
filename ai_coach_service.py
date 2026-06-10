d128c20a3353a896e03e3682a91ea4b3c1e4bdd6
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-10 14:27:47 +0530

    Update ai_coach_service.py

--- ai_coach_service.py
+++ ai_coach_service.py
@@ -1,10 +1,10 @@
-# Groq AI coach
 def get_tip(balance):
 
     if balance < 500:
         return "Try reducing food delivery expenses."
 
-    if balance < 1000:
+    elif balance < 1000:
         return "Good job. Save a little more every week."
 
-    return "Excellent savings habit."
+    else:
+        return "Excellent savings habit. Keep it up!"
