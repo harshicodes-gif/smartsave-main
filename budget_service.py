0f604d4724cb6feba5fe1d2d626cd017437fa4ad
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-13 08:59:20 +0530

    Add functions to get and save pocket money

--- budget_service.py
+++ budget_service.py
@@ -75,3 +75,55 @@ def delete_transaction(transaction_id):
     conn.commit()
 
     conn.close()
+
+
+def get_pocket_money(username):
+
+    conn = get_connection()
+
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT pocket_money
+        FROM user_settings
+        WHERE username = ?
+        """,
+        (username,)
+    )
+
+    row = cur.fetchone()
+
+    conn.close()
+
+    if row:
+
+        return row[0]
+
+    return 5000.0
+
+
+def save_pocket_money(
+    username,
+    pocket_money
+):
+
+    conn = get_connection()
+
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        UPDATE user_settings
+        SET pocket_money = ?
+        WHERE username = ?
+        """,
+        (
+            pocket_money,
+            username
+        )
+    )
+
+    conn.commit()
+
+    conn.close()
