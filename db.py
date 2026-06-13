53de0ff787bbe6932798d6605324a2da987164ac
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-13 08:56:25 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,31 +2,51 @@ import sqlite3
 
 DB_NAME = "smartsave.db"
 
+
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+
+    return sqlite3.connect(
+        DB_NAME
+    )
+
 
 def init_db():
 
     conn = get_connection()
+
     cur = conn.cursor()
 
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password TEXT
+        )
+        """
     )
-    """)
 
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS transactions(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        category TEXT,
-        amount REAL,
-        description TEXT
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS transactions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            category TEXT,
+            amount REAL,
+            description TEXT
+        )
+        """
     )
-    """)
+
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS user_settings(
+            username TEXT PRIMARY KEY,
+            pocket_money REAL DEFAULT 5000
+        )
+        """
+    )
 
     conn.commit()
+
     conn.close()
