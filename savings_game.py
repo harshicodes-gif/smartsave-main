5763f05f0ba97c0c39d19f1fe90e1eec052ac372
harshicodes-gif <f20242048@hyderabad.bits-pilani.ac.in>
2026-06-27 16:03:32 +0530

    Update savings_game.py

--- savings_game.py
+++ savings_game.py
@@ -1,73 +1,58 @@
 import streamlit as st
-
 from translations import translations
 
 
 def show_game():
-
     language = st.session_state.language
     t = translations[language]
 
-    st.header(
-        t["savings_game"]
+    st.header(t["savings_game"])
+
+    st.markdown(
+        """
+        <style>
+        button[data-testid="stNumberInputStepUp"] {
+            display: none !important;
+        }
+
+        button[data-testid="stNumberInputStepDown"] {
+            display: none !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
     )
 
-    st.subheader(
-        t["savings_challenge"]
-    )
+    st.subheader(t["savings_challenge"])
 
     saved = st.number_input(
         t["saved_amount"],
-        min_value=0.0
+        min_value=0.0,
     )
 
     if saved < 1000:
-
         level = t["bronze"]
-
     elif saved < 5000:
-
         level = t["silver"]
-
     else:
-
         level = t["gold"]
 
     current_level_text = {
-        "English":
-            f"Current Level: {level}",
-
-        "Hindi":
-            f"वर्तमान स्तर: {level}",
-
-        "Telugu":
-            f"ప్రస్తుత స్థాయి: {level}"
+        "English": f"Current Level: {level}",
+        "Hindi": f"वर्तमान स्तर: {level}",
+        "Telugu": f"ప్రస్తుత స్థాయి: {level}",
     }[language]
 
-    st.success(
-        current_level_text
-    )
+    st.success(current_level_text)
 
-    progress = min(
-        saved / 5000,
-        1.0
-    )
+    progress = min(saved / 5000, 1.0)
 
-    st.progress(
-        progress
-    )
+    st.progress(progress)
 
     progress_text = {
-        "English":
-            f"Progress: {progress * 100:.0f}%",
-
-        "Hindi":
-            f"प्रगति: {progress * 100:.0f}%",
-
-        "Telugu":
-            f"పురోగతి: {progress * 100:.0f}%"
+        "English": f"Progress: {progress * 100:.0f}%",
+        "Hindi": f"प्रगति: {progress * 100:.0f}%",
+        "Telugu": f"పురోగతి: {progress * 100:.0f}%",
     }[language]
 
-    st.caption(
-        progress_text
-    )
+    st.caption(progress_text)
